b76533ff631aa7de11bbe749f707fa8ff25e4f7a
e-chevalier <echevalier.dev@gmail.com>
2022-01-17 11:53:46 -0300

    Clase 4 - Practica 6

--- practica6/server.js
+++ practica6/server.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import multer from 'multer'
+
 const app = express()
 const router = express.Router()
 
@@ -6,39 +8,50 @@ const PORT = 8080
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-app.use('/personas', router)
-app.use('/mascotas', router)
 app.use(express.static('public'))
 
 
+let storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now()+'-'+file.originalname )
+    }
+})
+
+let upload = multer({storage: storage })
+
+
+
 const server = app.listen(PORT, () => {
-    console.log(`Servidor http escuchando en el puerto ${server.address().port}`)
+    console.log(`Servidor http escuchando en el puerto ${server.address().port}
+                 Open link to http://127.0.0.1:${server.address().port}`)
 })
 
 server.on("error", error => console.log(`Error en servidor ${error}`))
 
-const personas = [{nombre: 'Esteban', apellido: 'Chevalier', edad: '39'}]
-const mascotas = [{nombre: 'Kaisa', raza: 'generico', edad: '3' }]
-
-
-// ROUTER GET
-
-router.get('/',  (req, res) => {
-    console.log(`GET Request received from ${req.originalUrl}`);
-    let origin = req.originalUrl.slice(1)
-    res.json( origin === 'mascotas'? mascotas: personas)
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + 'index.html')
 })
 
-
-// ROUTER POST
-
-router.post('/',  (req, res) => {
-    console.log(`POST Request received from ${req.originalUrl}`);
-    let origin = req.originalUrl.slice(1)
-    origin === 'mascotas' ? mascotas.push(req.body):personas.push(req.body)
-    res.json( origin === 'mascotas'? mascotas: personas )
+app.post('/uploadfile', upload.single('myFile'), (req, res, next) => {
+    const file = req.file
+    if(!file) {
+        const error = new Error('Please upload a File')
+        error.httpStatusCode = 400
+        return next(error)
+    }
+    res.send(file)
 })
 
-
-
+app.post('/uploadmultiple', upload.array('myFiles', 12), (req, res, next) => {
+    const files = req.files
+    if(!files) {
+        const error = new Error('Please choose files')
+        error.httpStatusCode = 400
+        return next(error)
+    }
+    res.send(files)
+})
 
